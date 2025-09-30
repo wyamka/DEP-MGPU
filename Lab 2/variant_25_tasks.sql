b3c44f5fbb6ec70e50ce41890180bdfd180e13ae
wyamka <daisy_chamomile@mail.ru>
2025-09-30 09:51:15 +0300

    Update and rename Задание 1_ Вар 25 to variant_25_tasks.sql

---
+++ Lab 2/variant_25_tasks.sql
@@ -0,0 +1,30 @@
+-- Задание 1: [Создать представление по регионам]
+CREATE VIEW dw.region AS
+SELECT 
+    o.region,
+    o.country,
+    SUM(f.sales) AS total_sales,
+    SUM(f.profit) AS total_profit,
+    SUM(f.quantity) AS total_quantity,
+    AVG(f.discount) AS avg_discount
+FROM dw.sales_fact f
+JOIN stg.orders o 
+    ON f.order_id = o.order_id
+GROUP BY o.region, o.country;
+
+
+
+-- Задание 2: [Определить возвраты по менеджерам]
+CREATE TABLE dw.return_people AS
+SELECT 
+    p.person AS manager,
+    p.region,
+    COUNT(r.order_id) AS total_returns,
+    SUM(o.sales) AS returned_sales,
+    SUM(o.profit) AS returned_profit
+FROM public.returns r
+JOIN stg.orders o 
+    ON r.order_id = o.order_id
+JOIN public.people p
+    ON o.region = p.region
+GROUP BY p.person, p.region;
